b569ef60e57a586ddbf7ec1205bf1aabe305162c
Renan Garcia <renanrgarcia@gmail.com>
2024-07-01 12:29:00 -0300

    section10 done

--- _classNotes/section10-Requests-Inputs-Flash.md
+++ _classNotes/section10-Requests-Inputs-Flash.md
@@ -6,3 +6,15 @@
 ## Method
 - `$request->flash()`: Returns an array of all the flash data in the request.
 - `$request->flashOnly('name', 'email')`: Returns an array of only the specified flash data in the request.
+- `$request->flashExcept('name', 'email')`: Returns an array of all flash data except the specified flash data in the request.
+
+## Redirect
+- `redirect()->back()`: Redirects back to the previous page.
+- `redirect('/')->withInput()`: Redirects to the '/' route with the input data.
+- `back()->withInput()`: Redirects back to the previous page with the input data.
+- `$request->cookie('name')`: Returns the value of the cookie with the name `name`. 
+- `$request->route('home')->withInput()`: Redirects to the 'home' route with the input data.
+
+### Retrieve the data on form
+- `<input ... value="{{ old('name') }}">`: Returns the value of the input with the name `name` from the request.
+- On the route: `$request->old('name')`
--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -7,44 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <!-- <form action="/colors" method="POST">
+   
+    <form action="/flash" method="post">
         @csrf
-        
-        <input type="text" name="colors[]" value="red">
-        <input type="text" name="colors[]" value="green">
-        <input type="text" name="colors[]" value="blue">
-        <input type="text" name="colors[]" value="yellow">
+        <input type="email" name="email" placeholder="Enter your email" value="{{ old('email') }}">
+        <input type="text" name="username" placeholder="Enter your username">
         <button type="submit">Submit</button>
-    </form> -->
-
-    <!-- <hr>
-
-    <form action="/date" method="post">
-        @csrf
-        <input type="date" name="appointment">
-        <button type="submit">Submit</button>
-    </form> -->
-    <div class="container">
-        <div class="col-4">  
-            <form action="/custom" method="post">
-                @csrf
-                <div class="mb-3 mt-4">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input required name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input name="password" type="password" class="form-control" id="exampleInputPassword1">
-                </div>
-                <div class="mb-3 form-check">
-                    <input name="checkBox" type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>  
-    </div>
+    </form>
     
 </body>
 </html>
